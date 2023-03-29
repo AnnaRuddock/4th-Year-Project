e3b9f02d0ffff93ff68146a9ea6d060945339554
AnnaRuddock <87811202+AnnaRuddock@users.noreply.github.com>
2023-03-29 22:07:18 +0100

    Update NBinVsPoissGraph.m

--- NBinVsPoissGraph.m
+++ NBinVsPoissGraph.m
@@ -1,6 +1,8 @@
-x = 0:50;
+set(0,'defaultaxesfontsize',12)
+
+x = 0:15;
 plot(x,nbinpdf(x,1.024,.3125), ...
-     x,poisspdf(x,2.2528));
-legend({'Negative Binomial' 'Poisson'})
+     x,poisspdf(x,2.2528), x,geopdf(x,0.3074));
+legend({'Negative Binomial (1.024, 0.3125) Distribution' 'Poisson (2.2528) Distribution', 'Geometric(0.3074) Distribution'})
 xlabel('Number of secondary cases')
 ylabel('Probability')
