22e5c24046cce0ec254a8737151b8611c8dac0af
AnnaRuddock <87811202+AnnaRuddock@users.noreply.github.com>
2023-04-03 15:21:27 +0100

    Delete NBinVsPoissGraph.m

--- NBinVsPoissGraph.m
+++
@@ -1,8 +0,0 @@
-set(0,'defaultaxesfontsize',12)
-
-x = 0:15;
-plot(x,nbinpdf(x,1.024,.3125), ...
-     x,poisspdf(x,2.2528), x,geopdf(x,0.3074));
-legend({'Negative Binomial (1.024, 0.3125) Distribution' 'Poisson (2.2528) Distribution', 'Geometric(0.3074) Distribution'})
-xlabel('Number of secondary cases')
-ylabel('Probability')
