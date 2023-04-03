06c2f199e787df67ba76ca02d1c46aad8fde013f
AnnaRuddock <87811202+AnnaRuddock@users.noreply.github.com>
2023-04-03 15:19:48 +0100

    Update twohostsolverebola.m

--- twohostsolverebola.m
+++ twohostsolverebola.m
@@ -1,3 +1,4 @@
+% Inputting vector of parameter values
 P = [6.4,0.019,4,0.16*2.2,0.72,0.25,6.1,0.3,0.4744];
 
 % Solving using the PGF method to get the expected outbreak probabilities
@@ -8,6 +9,7 @@ options = optimoptions('fsolve','Display','none');
 x = fsolve(fun,x0, options);
 x = 1.-x
 
+% Now solving with the first step analysis method to estimate the same probabilities
 fun = @(r)firststepmethodebola(r,P);
 y0 = [0 0];
 y = fsolve(fun, y0, options);
