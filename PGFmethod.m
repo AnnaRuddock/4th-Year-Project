9479c64eee0a8ee35de22b61ec14900402a78e10
AnnaRuddock <87811202+AnnaRuddock@users.noreply.github.com>
2023-04-03 15:23:19 +0100

    Update PGFmethod.m

--- PGFmethod.m
+++ PGFmethod.m
@@ -1,7 +1,9 @@
 function F = PGFmethod(q)
 
+% Inputting the NGM (from the data from an influenza outbreak in Mexico)
 K = [1.41, 0.34; 0.35, 0.87];
 
+% Writing the 2 equations that q(1) and q(2) must satisfy
 F(1) = q(1) - 1/(K(1,1)*(1-q(1)) + K(1,2)*(1-q(2))+1);
 F(2) = q(2) - 1/(K(2,1)*(1-q(1)) + K(2,2)*(1-q(2))+1);
 
