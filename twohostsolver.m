1d3a0fd847a6d68690c2b4ad69def8cf89dff0ae
AnnaRuddock <87811202+AnnaRuddock@users.noreply.github.com>
2023-04-03 15:30:25 +0100

    Update twohostsolver.m

--- twohostsolver.m
+++ twohostsolver.m
@@ -1,8 +1,13 @@
+% Importing the PGF method function
 fun = @PGFmethod;
 x0 = [0,0];
+% Changing options so no display shows in the command window when functions are solved
 options = optimoptions('fsolve','Display','none');
+% Using fsolve to find a vector that satisfied the equations in PGFmethod
 x = fsolve(fun,x0, options)
 
+% Importing the first step method function
 fun = @firststepmethod;
 y0 = [0,0];
+% Using fsolve to find a vector that satisfied the equations in firststepmethod
 y = fsolve(fun,y0, options)
